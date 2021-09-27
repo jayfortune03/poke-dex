3a323ee6a1c28f1fe4d756bdfa6167c509529979
Nicholas Fortune <nfortune03@gmail.com>
2021-09-27 13:43:33 +0700

    setup fetching all pokemons

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
-import Card from "./components/Card";
 import "./App.css";
 
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import Card from "./components/Card";
+
+import { fetchPokemons } from "./store/action";
+
 function App() {
+  const dispatch = useDispatch();
+  const pokemons = useSelector((state) => state.pokemons);
+
+  useEffect(() => {
+    dispatch(fetchPokemons());
+  }, [dispatch]);
+
+  console.log(pokemons);
+
   return (
     <div className="mx-96 my-96">
       <Card />
